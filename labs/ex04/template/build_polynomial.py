ec1e0afea8a4476d5fa10c5e5a32a9f8a98885ab
Ke <ke.li@epfl.ch>
2023-10-17 16:12:35 +0200

    week4

--- labs/ex04/template/build_polynomial.py
+++ labs/ex04/template/build_polynomial.py
@@ -18,10 +18,8 @@ def build_poly(x, degree):
     array([[1.  , 0.  , 0.  ],
            [1.  , 1.5 , 2.25]])
     """
-    # ***************************************************
-    # COPY YOUR CODE FROM EX03 HERE
-    # polynomial basis function: TODO
-    # this function should return the matrix formed
-    # by applying the polynomial basis to the input data
-    # ***************************************************
-    raise NotImplementedError
+    N = x.shape[0]
+    poly = np.ones((N, degree+1))
+    for i in range(1, degree+1):
+        poly[:, i] = x**i
+    return poly
--- labs/ex04/template/ridge_regression.py
+++ labs/ex04/template/ridge_regression.py
@@ -23,8 +23,8 @@ def ridge_regression(y, tx, lambda_):
     >>> ridge_regression(np.array([0.1,0.2]), np.array([[2.3, 3.2], [1., 0.1]]), 1)
     array([0.03947092, 0.00319628])
     """
-    # ***************************************************
-    # COPY YOUR CODE FROM EX03 HERE
-    # ridge regression: TODO
-    # ***************************************************
-    raise NotImplementedError
+
+    N = tx.shape[0]
+    D = tx.shape[1]
+    w = np.linalg.solve(tx.T.dot(tx) + 2 * N* lambda_ * np.eye(D), tx.T.dot(y))
+    return w
